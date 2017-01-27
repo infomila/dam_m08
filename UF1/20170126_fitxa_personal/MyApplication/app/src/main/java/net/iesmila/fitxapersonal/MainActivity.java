ab44ed2c5911e69c7bffdec4fcba0ae6fdbea3b2
Bernat Orellana <borellan@xtec.cat>
2017-01-27 18:00:45 +0100

    asdf

--- UF1/20170126_fitxa_personal/MyApplication/app/src/main/java/net/iesmila/fitxapersonal/MainActivity.java
+++ UF1/20170126_fitxa_personal/MyApplication/app/src/main/java/net/iesmila/fitxapersonal/MainActivity.java
@@ -1,17 +1,51 @@
 package net.iesmila.fitxapersonal;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends Activity {
+    ImageView mImvCara;
+    ImageView mBtnCamera;
+    EditText mEdtNom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //-----------------------------------------------------
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //-----------------------------------------------------
+        mImvCara = (ImageView) findViewById(R.id.imvCara);
+        mBtnCamera = (ImageView) findViewById(R.id.btnCamera);
+        mEdtNom = (EditText) findViewById(R.id.edtNom);
+
+        //-----------------------------------------------------
+        mBtnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+        //--------------------------------------------------
+        mEdtNom.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {}
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {}
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String elQueHiHaEnElEditText = editable.toString();
+            }
+        });
     }
 
 
@@ -35,5 +69,25 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //---------------------------------------------------------
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            mImvCara.setImageBitmap(imageBitmap);
+        }
     }
 }
